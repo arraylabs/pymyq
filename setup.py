e7ba8a3660f9cbfd529b0bf4e02cdefdf46007b2
arraylabs <chris@arraylabs.com>
2017-02-11 18:27:57 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pymyq',
+    version='0.0.1',
+    description='Python package for controlling MyQ-Enabled Garage Door',
+    author='Chris Campbell',
+    author_email='chris@arraylabs.com',
+    url='https://github.com/arraylabs/pymyq',
+    license=license,
+    packages=['pymyq'],
+    package_dir={'pymyq': 'pymyq'}
+)
