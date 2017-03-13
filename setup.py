d920d83a2497d893a97bb7c2b8f098ec7aeb15f3
ArrayLabs <chris@arraylabs.com>
2017-03-13 16:12:39 -0400

    update pymyq to 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pymyq',
-    version='0.0.7',
+    version='0.0.8',
     description='Python package for controlling MyQ-Enabled Garage Door',
     author='Chris Campbell',
     author_email='chris@arraylabs.com',
