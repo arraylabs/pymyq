4c4eea4926f4d0df26ac8284f12b07620b1e414d
Erik Hendrix <hendrix_erik@hotmail.com>
2021-02-05 16:47:12 -0700

    Fixed requirements in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [  # type: ignore
-    'aiohttp',
+    'aiohttp', 'beautifulsoup4', 'pkce'
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -117,9 +117,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
