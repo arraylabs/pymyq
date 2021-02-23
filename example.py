5f9c43953c31636e2f2590e83b4d720448083533
Erik Hendrix <hendrix_erik@hotmail.com>
2021-02-23 16:11:44 -0700

    pre-commit fixes

--- example.py
+++ example.py
@@ -5,8 +5,10 @@ import logging
 from aiohttp import ClientSession
 
 from pymyq import login
+from pymyq.account import MyQAccount
+from pymyq.api import API
 from pymyq.errors import MyQError, RequestError
-from pymyq.garagedoor import STATE_OPEN, STATE_CLOSED
+from pymyq.garagedoor import STATE_CLOSED, STATE_OPEN
 
 _LOGGER = logging.getLogger()
 
@@ -16,6 +18,12 @@ ISSUE_COMMANDS = False
 
 
 def print_info(number: int, device):
+    """Print the device information
+
+    Args:
+        number (int): [description]
+        device ([type]): [description]
+    """
     print(f"      Device {number + 1}: {device.name}")
     print(f"      Device Online: {device.online}")
     print(f"      Device ID: {device.device_id}")
@@ -34,6 +42,127 @@ def print_info(number: int, device):
     print("      ---------")
 
 
+async def print_garagedoors(api: API, account: MyQAccount):
+    """Print garage door information and open/close if requested
+
+    Args:
+        api (API): API object for the connection
+        account (MyQAccount): Account for which to retrieve garage doors
+    """
+    print(f"  GarageDoors: {len(api.covers)}")
+    print("  ---------------")
+    if len(api.covers) != 0:
+        for idx, device_id in enumerate(
+            device_id
+            for device_id in api.covers
+            if api.devices[device_id].account == account
+        ):
+            device = api.devices[device_id]
+            print_info(number=idx, device=device)
+
+            if ISSUE_COMMANDS:
+                try:
+                    if device.open_allowed:
+                        if device.state == STATE_OPEN:
+                            print(f"Garage door {device.name} is already open")
+                        else:
+                            print(f"Opening garage door {device.name}")
+                            try:
+                                if await device.open(wait_for_state=True):
+                                    print(f"Garage door {device.name} has been opened.")
+                                else:
+                                    print(f"Failed to open garage door {device.name}.")
+                            except MyQError as err:
+                                _LOGGER.error(
+                                    "Error when trying to open %s: %s",
+                                    device.name,
+                                    str(err),
+                                )
+                    else:
+                        print(f"Opening of garage door {device.name} is not allowed.")
+
+                    if device.close_allowed:
+                        if device.state == STATE_CLOSED:
+                            print(f"Garage door {device.name} is already closed")
+                        else:
+                            print(f"Closing garage door {device.name}")
+                            wait_task = None
+                            try:
+                                wait_task = await device.close(wait_for_state=False)
+                            except MyQError as err:
+                                _LOGGER.error(
+                                    "Error when trying to close %s: %s",
+                                    device.name,
+                                    str(err),
+                                )
+
+                            print(f"Device {device.name} is {device.state}")
+
+                            if (
+                                wait_task
+                                and isinstance(wait_task, asyncio.Task)
+                                and await wait_task
+                            ):
+                                print(f"Garage door {device.name} has been closed.")
+                            else:
+                                print(f"Failed to close garage door {device.name}.")
+
+                except RequestError as err:
+                    _LOGGER.error(err)
+        print("  ------------------------------")
+
+
+async def print_lamps(api: API, account: MyQAccount):
+    """Print lamp information and turn on/off if requested
+
+    Args:
+        api (API): API object for the connection
+        account (MyQAccount): Account for which to retrieve lamps
+    """
+    print(f"  Lamps: {len(api.lamps)}")
+    print("  ---------")
+    if len(api.lamps) != 0:
+        for idx, device_id in enumerate(
+            device_id
+            for device_id in api.lamps
+            if api.devices[device_id].account == account
+        ):
+            device = api.devices[device_id]
+            print_info(number=idx, device=device)
+
+            if ISSUE_COMMANDS:
+                try:
+                    print(f"Turning lamp {device.name} on")
+                    await device.turnon(wait_for_state=True)
+                    await asyncio.sleep(5)
+                    print(f"Turning lamp {device.name} off")
+                    await device.turnoff(wait_for_state=True)
+                except RequestError as err:
+                    _LOGGER.error(err)
+        print("  ------------------------------")
+
+
+async def print_gateways(api: API, account: MyQAccount):
+    """Print gateways for account
+
+    Args:
+        api (API): API object for the connection
+        account (MyQAccount): Account for which to gateways
+    """
+    print(f"  Gateways: {len(api.gateways)}")
+    print("  ------------")
+    if len(api.gateways) != 0:
+        for idx, device_id in enumerate(
+            device_id
+            for device_id in api.gateways
+            if api.devices[device_id].account == account
+        ):
+            device = api.devices[device_id]
+            print_info(number=idx, device=device)
+
+    print("------------------------------")
+
+
 async def main() -> None:
     """Create the aiohttp session and run the example."""
     logging.basicConfig(level=logging.DEBUG)
@@ -49,117 +178,11 @@ async def main() -> None:
 
                 # Get all devices listed with this account – note that you can use
                 # api.covers to only examine covers or api.lamps for only lamps.
-                print(f"  GarageDoors: {len(api.covers)}")
-                print("  ---------------")
-                if len(api.covers) != 0:
-                    for idx, device_id in enumerate(
-                        device_id
-                        for device_id in api.covers
-                        if api.devices[device_id].account == account
-                    ):
-                        device = api.devices[device_id]
-                        print_info(number=idx, device=device)
+                await print_garagedoors(api=api, account=account)
 
-                        if ISSUE_COMMANDS:
-                            try:
-                                if device.open_allowed:
-                                    if device.state == STATE_OPEN:
-                                        print(
-                                            f"Garage door {device.name} is already open"
-                                        )
-                                    else:
-                                        print(f"Opening garage door {device.name}")
-                                        try:
-                                            if await device.open(wait_for_state=True):
-                                                print(
-                                                    f"Garage door {device.name} has been opened."
-                                                )
-                                            else:
-                                                print(
-                                                    f"Failed to open garage door {device.name}."
-                                                )
-                                        except MyQError as err:
-                                            _LOGGER.error(
-                                                "Error when trying to open %s: %s",
-                                                device.name,
-                                                str(err),
-                                            )
-                                else:
-                                    print(
-                                        f"Opening of garage door {device.name} is not allowed."
-                                    )
+                await print_lamps(api=api, account=account)
 
-                                if device.close_allowed:
-                                    if device.state == STATE_CLOSED:
-                                        print(
-                                            f"Garage door {device.name} is already closed"
-                                        )
-                                    else:
-                                        print(f"Closing garage door {device.name}")
-                                        wait_task = None
-                                        try:
-                                            wait_task = await device.close(
-                                                wait_for_state=False
-                                            )
-                                        except MyQError as err:
-                                            _LOGGER.error(
-                                                "Error when trying to close %s: %s",
-                                                device.name,
-                                                str(err),
-                                            )
-
-                                        print(f"Device {device.name} is {device.state}")
-
-                                        if (
-                                            wait_task
-                                            and isinstance(wait_task, asyncio.Task)
-                                            and await wait_task
-                                        ):
-                                            print(
-                                                f"Garage door {device.name} has been closed."
-                                            )
-                                        else:
-                                            print(
-                                                f"Failed to close garage door {device.name}."
-                                            )
-
-                            except RequestError as err:
-                                _LOGGER.error(err)
-                    print("  ------------------------------")
-                print(f"  Lamps: {len(api.lamps)}")
-                print("  ---------")
-                if len(api.lamps) != 0:
-                    for idx, device_id in enumerate(
-                        device_id
-                        for device_id in api.lamps
-                        if api.devices[device_id].account == account
-                    ):
-                        device = api.devices[device_id]
-                        print_info(number=idx, device=device)
-
-                        if ISSUE_COMMANDS:
-                            try:
-                                print(f"Turning lamp {device.name} on")
-                                await device.turnon(wait_for_state=True)
-                                await asyncio.sleep(5)
-                                print(f"Turning lamp {device.name} off")
-                                await device.turnoff(wait_for_state=True)
-                            except RequestError as err:
-                                _LOGGER.error(err)
-                    print("  ------------------------------")
-
-                print(f"  Gateways: {len(api.gateways)}")
-                print("  ------------")
-                if len(api.gateways) != 0:
-                    for idx, device_id in enumerate(
-                        device_id
-                        for device_id in api.gateways
-                        if api.devices[device_id].account == account
-                    ):
-                        device = api.devices[device_id]
-                        print_info(number=idx, device=device)
-
-                print("------------------------------")
+                await print_gateways(api=api, account=account)
 
         except MyQError as err:
             _LOGGER.error("There was an error: %s", err)
